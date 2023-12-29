76a5ff694e07351e83da40270b2195447bc647b6
Taylor <taylorlauck%@gmail.com>
2023-12-28 22:16:19 -0500

    changed url

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend URL
+  origin: 'http://visualartexplorer.netlify.app', // Replace with your frontend URL
   methods: ['GET', 'POST', 'DELETE'],
   credentials: true,
 }));
