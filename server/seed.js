375bf98e04ac355dc79b22a54c837deacaddab76
Taylor <taylorlauck%@gmail.com>
2023-12-29 20:35:12 -0500

    added a table to my database

--- server/seed.js
+++ server/seed.js
@@ -20,20 +20,32 @@ async function createTables() {
 
       
 
+// Create artworks table and sequence
+await pool.query(`
+CREATE SEQUENCE artworks_artwork_id_seq;
+CREATE TABLE artworks (
+    artwork_id VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT nextval('artworks_artwork_id_seq'),
+    title VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL,
+    thumbnailurl TEXT NOT NULL
+);
+`);
+
+
         // Create favorites table
-        await pool.query(`
-            CREATE TABLE favorites (
-                username VARCHAR(50) NOT NULL,
-                artworkid VARCHAR(500) NOT NULL,
-                slug VARCHAR(255) NOT NULL,
-                title VARCHAR(255) NOT NULL,
-                image_url VARCHAR(500) NOT NULL,
-                CONSTRAINT username_artwork_unique UNIQUE (username, artworkid),
-                CONSTRAINT favorites_username_fkey FOREIGN KEY (username) REFERENCES accounts(username)
-            );
-        `);
-        console.log('Favorites table created in ElephantSQL.');
-    } catch (error) {
+       // await pool.query(`
+       //     CREATE TABLE favorites (
+           //     username VARCHAR(50) NOT NULL,
+             //   artworkid VARCHAR(500) NOT NULL,
+              //  slug VARCHAR(255) NOT NULL,
+              //  title VARCHAR(255) NOT NULL,
+              //  image_url VARCHAR(500) NOT NULL,
+              //  CONSTRAINT username_artwork_unique UNIQUE (username, artworkid),
+               // CONSTRAINT favorites_username_fkey FOREIGN KEY (username) REFERENCES accounts(username)
+          //  );
+      //  `);
+     //   console.log('Favorites table created in ElephantSQL.');
+  //  } catch (error) {
         console.error('Error creating tables:', error);
     } finally {
         pool.end();
