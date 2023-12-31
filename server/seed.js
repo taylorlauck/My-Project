aa0bb189a011c1d1236b0f049a8eacc59c25436e
Taylor <taylorlauck%@gmail.com>
2023-12-30 19:41:59 -0500

    updated seed.js

--- server/seed.js
+++ server/seed.js
@@ -6,17 +6,17 @@ const pool = require('./database');
 
 async function createTables() {
     try {
-        // Create accounts table
-        //await pool.query(`
-         //   CREATE TABLE accounts (
-             //   username VARCHAR(50) NOT NULL,
-             //   password VARCHAR(72) NOT NULL,
-              //  CONSTRAINT accounts_password_key UNIQUE (password),
-              //  CONSTRAINT accounts_username_key UNIQUE (username)
-           // );
-       // `);
-     //
-     //   console.log('Accounts table created in ElephantSQL.');
+         //Create accounts table
+        await pool.query(`
+           CREATE TABLE accounts (
+               username VARCHAR(50) NOT NULL,
+                password VARCHAR(72) NOT NULL,
+                CONSTRAINT accounts_password_key UNIQUE (password),
+                CONSTRAINT accounts_username_key UNIQUE (username)
+            );
+        `);
+     
+        console.log('Accounts table created in ElephantSQL.');
 
       
 
@@ -25,27 +25,24 @@ await pool.query(`
 CREATE SEQUENCE artworks_artwork_id_seq;
 CREATE TABLE artworks (
     artwork_id VARCHAR(255) PRIMARY KEY NOT NULL DEFAULT nextval('artworks_artwork_id_seq'),
-    title VARCHAR(255) NOT NULL,
     slug VARCHAR(255) NOT NULL,
-    thumbnailurl TEXT NOT NULL
+  
 );
 `);
 
 
         // Create favorites table
-       // await pool.query(`
-       //     CREATE TABLE favorites (
-           //     username VARCHAR(50) NOT NULL,
-             //   artworkid VARCHAR(500) NOT NULL,
-              //  slug VARCHAR(255) NOT NULL,
-              //  title VARCHAR(255) NOT NULL,
-              //  image_url VARCHAR(500) NOT NULL,
-              //  CONSTRAINT username_artwork_unique UNIQUE (username, artworkid),
-               // CONSTRAINT favorites_username_fkey FOREIGN KEY (username) REFERENCES accounts(username)
-          //  );
-      //  `);
-     //   console.log('Favorites table created in ElephantSQL.');
-  //  } catch (error) {
+        await pool.query(`
+            CREATE TABLE favorites (
+                username VARCHAR(50) NOT NULL,
+                artworkid VARCHAR(500) NOT NULL,
+               slug VARCHAR(255) NOT NULL,
+                CONSTRAINT username_artwork_unique UNIQUE (username, artworkid),
+                CONSTRAINT favorites_username_fkey FOREIGN KEY (username) REFERENCES accounts(username)
+            );
+        `);
+        console.log('Favorites table created in ElephantSQL.');
+    } catch (error) {
         console.error('Error creating tables:', error);
     } finally {
         pool.end();
